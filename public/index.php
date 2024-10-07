619bf916775d408b5d5e126a89e352219ee88b40
aliabdulbaqi01 <aliabdulbaqi01@gmail.com>
2024-10-07 21:23:43 +0300

    start working on mvc structure instead of procedural structure

--- public/index.php
+++ public/index.php
@@ -1,22 +1,6 @@
 <?php
-// index.php
 
-require_once __DIR__ . "/../src/bootstrap.php";
-
-// Set the layout
-Template::extend('dashboard.php');
-
-// Start defining the title section
-Template::section('title');
-echo 'Home Page';
-Template::endSection();
-
-// Start defining the content section
-Template::section('content');
-?>
-
-<?php
-Template::endSection();
-
-// Render the final output
-Template::render();
+require_once  __DIR__ . '/../vendor/autoload.php';
+//require_once  __DIR__ . '/../routes/web.php';
+echo 'hello';
+dump($_SERVER);
